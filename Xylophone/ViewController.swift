1bdd13b490a9d68b0478b1270f3df3bdcc57a4b6
rasyidrw <rasyidrespati.30@gmail.com>
2019-10-24 17:10:09 +0700

    added asyncAfter for delaying background opacity 0.2s

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -4,19 +4,29 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var player: AVAudioPlayer!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound()
+        playSound(soundName: sender.currentTitle!)
+        
+        print("Start")
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + (0.2), execute: {
+            sender.alpha = 1
+            print("End")
+        })
+        
+        
     }
     
-    func playSound() {
-        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
-                
+        
     }
 }
